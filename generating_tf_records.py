More mug fixes in regression delta calculation

generating_tf_records.py
@@ -1,20 +1,17 @@
 import numpy as np
 import tensorflow as tf
 import os
-import re
 import glob
 from box_overlaps import bbox_overlaps
 from matplotlib import pyplot as plt
 import shutil
-from skimage.transform import resize
+import constants as const
 
 
 def generating_probs_maps(anchor_size, boxes, feature_map_shape, scale_factor):
     anchor_size_pad = int(anchor_size / 2)
     objboxes = corner_to_standup_box2d(boxes, scale_factor)
     anchors = create_anchors(anchor_size, feature_map_shape)
-
-    # draw_bbox2D(data[4], objboxes)
 
     pos_equal_one = np.zeros(feature_map_shape)
     neg_equal_one = np.zeros(feature_map_shape)
@@ -38,7 +35,6 @@ def generating_probs_maps(anchor_size, boxes, feature_map_shape, scale_factor):
     id_pos_gt = np.concatenate([id_pos_gt, id_highest_gt])
 
     id_pos, index = np.unique(id_pos, return_index=True)
-    id_pos_gt = id_pos_gt[index]
     id_neg.sort()
 
     # cal the target and set the equal one
@@ -60,7 +56,7 @@ def generating_probs_maps(anchor_size, boxes, feature_map_shape, scale_factor):
 
 def corner_to_standup_box2d(boxes_corner, scale_factor):
     N = len(boxes_corner)
-    boxes2d = np.zeros((N,4))
+    boxes2d = np.zeros((N, 4))
     for i in range(N):
         x, y, z, l, w, h = boxes_corner[i].reshape((6, 1))
         boxes2d[i, :] = (x / scale_factor - l / (scale_factor*2), y / scale_factor - w / (scale_factor*2), \
@@ -79,35 +75,29 @@ def create_anchors(anchor_size, feature_map_shape):
     return anchors
 
 
-def get_regression_deltas(pos_equal_one, bboxes, anchor_size, scale_reduction):
-    
+def get_regression_deltas(pos_equal_one, bboxes, anchor_size, scale):
     h, w = pos_equal_one.shape
     anchor_reg = np.zeros((h, w, 6))
+    bboxes = bboxes/scale
 
-    for i in range(bboxes.shape[0]):
-        bboxes[i] = (bboxes[i]/scale_reduction).astype(int)
-
-    X_Idx, Y_Idx = np.where(pos_equal_one==1)
+    X_Idx, Y_Idx = np.where(pos_equal_one == 1)
     for i in range(len(X_Idx)):
         distances = []
         for j in range(bboxes.shape[0]):
-            dist = np.linalg.norm(np.array(bboxes[i][0], bboxes[i][1]) - np.array(X_Idx[i], Y_Idx[i]))
-            distances.append(dist)
-        min_idx = int(np.min(distances))
-        anchor_reg[X_Idx[i], Y_Idx[i], :] = np.array((bboxes[min_idx])) - np.array((X_Idx[i],  Y_Idx[i],_, anchor_size, anchor_size, anchor_size))
+            distances.append((bboxes[j][0] - X_Idx[i])**2 + (bboxes[j][1] - Y_Idx[i])**2)
+        min_idx = np.argmin(distances)
+        anchor_reg[X_Idx[i], Y_Idx[i], :] = bboxes[min_idx] - (X_Idx[i],  Y_Idx[i], const.default_z, anchor_size, anchor_size, anchor_size)
     
     return anchor_reg
 
 
 def controller_for_one_file(file_name):
-    anchor_size, scale_factor = 9, 4
     feature_map_shape = np.array((32, 32))
     images = []
     bbox_coordinates = []
     image_names = glob.glob(file_name + '/*.png')
     for j in range(len(image_names)):
-        img = plt.imread(image_names[j])
-        img = resize(img[:, :, :3], (128, 128))
+        img = plt.imread(image_names[j])[:, :, :3]
         images.append(img)
     data = np.load(file_name + '/bboordinates.npz')
     dims = data['dims']
@@ -117,8 +107,8 @@ def controller_for_one_file(file_name):
         l, w, h = dims[j]
         bbox_coordinates.append([x, y, z, l, w, h])
     bbox_coordinates = np.stack(bbox_coordinates)
-    pos_equal_one, neg_equal_one = generating_probs_maps(anchor_size, bbox_coordinates,feature_map_shape, scale_factor)
-    anchors_reg = get_regression_deltas(pos_equal_one, bbox_coordinates, anchor_size, scale_factor)
+    pos_equal_one, neg_equal_one = generating_probs_maps(const.anchor_size, bbox_coordinates,feature_map_shape, const.scale_factor)
+    anchors_reg = get_regression_deltas(pos_equal_one, bbox_coordinates, const.anchor_size, const.scale_factor)
     return images, bbox_coordinates, pos_equal_one, neg_equal_one, anchors_reg
 
 